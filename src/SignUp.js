a654eba29cfa9c1613daba73c5004d7cce2cf5f9
Franco Ortega <francopdx@gmail.com>
2020-11-11 15:44:46 -0800

    add initial state and form to SignUp.js

--- src/SignUp.js
+++ src/SignUp.js
@@ -1,10 +1,34 @@
 import React, { Component } from 'react'
 
 export default class SignUp extends Component {
+    state = {
+        username: '',
+        password: ''
+    }
+
+
+
+
     render() {
         return (
             <div>
                 SIGN UP PAGE!!!
+                <p>
+                    Fill out this lovely form to get your chores done. :)
+                </p>
+                <form>
+                    <label>
+                        Username:
+                        <input />
+                    </label>
+                    <label>
+                        Password:
+                        <input />
+                    </label>
+                    <button>
+                        Sign Up!
+                    </button>
+                </form>
             </div>
         )
     }
