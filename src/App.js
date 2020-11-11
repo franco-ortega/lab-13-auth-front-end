996d2d051a949b289b050b1798dadbbd2e307723
Franco Ortega <francopdx@gmail.com>
2020-11-11 15:37:19 -0800

    add additional info to app.js because I forgot to save again before the previous commit

--- src/App.js
+++ src/App.js
@@ -27,12 +27,12 @@ export default class App extends Component {
           <ul>
             { this.state.token && <div>welcome, user!!!</div> }
             { this.state.token && <Link to="/todos"><div>todos</div></Link> }
-            <Link to="/login"><div>log in</div></Link>
-            <Link to="/signup"><div>sign up</div></Link>
+            <Link to="/login"><div>Log In</div></Link>
+            <Link to="/signup"><div>Sign Up</div></Link>
             <button onClick={() => this.handleTokenChange('')}>logout</button>
           </ul>
           <Switch>
-            <Route exact path='/home' render={(routerProps) => <Home 
+            <Route exact path='/' render={(routerProps) => <Home 
                 handleTokenChange={this.handleTokenChange} 
                 {...routerProps} />} 
               />
