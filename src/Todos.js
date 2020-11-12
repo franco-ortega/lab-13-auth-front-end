328a3139e7523849dcce9ddda0fb56ad9e25bfba
Franco Ortega <francopdx@gmail.com>
2020-11-11 16:28:44 -0800

    add functionality to Todos.js

--- src/Todos.js
+++ src/Todos.js
@@ -1,10 +1,29 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import request from 'superagent';
 
 export default class Todos extends Component {
+    state = {
+        todos: []
+    }
+
+    componentDidMount = async () => {
+        const response = await request.get('https://still-chamber-35164.herokuapp.com/todos')
+        .set('Authorization', this.props.token)
+
+        this.setState({ todos: response.body })
+    }
+
+
     render() {
         return (
             <div>
-                TODOS PAGE with chores
+                Welcome to your Todos page of Chores.
+                {
+                    Boolean(this.state.todos.length) && this.state.todos.map(todo =>
+                        <div>
+                            name: {todo.chore}; completed: {todo.comleted}
+                        </div>)
+                }
             </div>
         )
     }
