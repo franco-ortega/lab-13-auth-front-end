40d22ac52df0d4e4930fabbf92b7ea2a826092a2
Franco Ortega <francopdx@gmail.com>
2020-11-11 18:06:11 -0800

    add full functionality to Todos.js

--- src/Todos.js
+++ src/Todos.js
@@ -3,14 +3,44 @@ import request from 'superagent';
 
 export default class Todos extends Component {
     state = {
-        todos: []
+        todos: [],
+        todoName: '',
+        loading: false
     }
 
     componentDidMount = async () => {
-        const response = await request.get('https://still-chamber-35164.herokuapp.com/todos')
+        await this.fetchTodos()
+    }
+
+    fetchTodos = async () => {
+        await this.setState({ loading: true });
+        const response = await request.get('https://still-chamber-35164.herokuapp.com/api/todos')
         .set('Authorization', this.props.token)
 
-        this.setState({ todos: response.body })
+        await this.setState({ todos: response.body, loading: false })
+    }
+
+    handleSubmitTodoForm = async (e) => {
+        e.preventDefault();
+
+        const newTodo = {
+            chore: this.state.todoName
+        };
+
+        await this.setState({ loading: true });
+
+        await request.post('https://still-chamber-35164.herokuapp.com/api/todos')
+        .send(newTodo)
+        .set('Authorization', this.props.token);
+
+        await this.fetchTodos();
+    }
+
+    handleClickChore = async (someId) => {
+        await request.put(`https://still-chamber-35164.herokuapp.com/api/todos/${someId}`)
+        .set('Authorization', this.props.token);
+
+        await this.fetchTodos();
     }
 
 
@@ -18,12 +48,39 @@ export default class Todos extends Component {
         return (
             <div>
                 Welcome to your Todos page of Chores.
+                <form onSubmit={this.handleSubmitTodoForm}>
+                    <label>
+                        Add a chore (you can always do more!!):
+                        <input
+                        value={this.state.todoName}
+                        onChange={(e) => this.setState({ todoName: e.target.value })}
+                        />
+                    </label>
+                    <button>
+                        Add Chore
+                    </button>
+                </form>
                 {
-                    Boolean(this.state.todos.length) && this.state.todos.map(todo =>
-                        <div>
-                            Chore: {todo.chore}; Done Yet?: {todo.comleted}
+                    this.state.loading
+                    ? '~ ~ ~ ~ ~ Loading ~ ~ ~ ~ ~'
+                    : this.state.todos.map(todo =>
+                        <div key={`${todo.chore}${todo.id}${Math.random()}`} style={{ textDecoration: todo.comleted ? 'line-through' : 'none'}}
+                        >
+                            Chore: {todo.chore}
+                            {
+                                todo.completed ? ' Well done! You did a chore!' : <button onClick={() => this.handleClickChore(todo.id)}>
+                                    Do Chore
+                                </button>
+                            }
                         </div>)
+
+
                 }
+
+
+
+
+
             </div>
         )
     }
