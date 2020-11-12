c944a3fa86e8a66fa1ce367b2129166ca8152dbe
Franco Ortega <francopdx@gmail.com>
2020-11-11 17:26:05 -0800

    add functionality to Login.js

--- src/Login.js
+++ src/Login.js
@@ -1,10 +1,60 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import request from 'superagent';
 
-export default class Login extends Component {
+export default class SignUp extends Component {
+    state = {
+        email: '',
+        password: '',
+        loading: false
+    }
+
+    handleSubmitLogin = async (e) => {
+        e.preventDefault();
+
+console.log(this.state);
+
+        this.setState({ loading: true })
+
+        const user = await request
+        .post('https://still-chamber-35164.herokuapp.com/auth/signin')
+        .send(this.state)
+
+console.log(user.body, 'sending signup info');
+
+        this.setState({ loading: false })
+
+        this.props.handleChangeUsernameAndToken(user.body.email, user.body.token)
+
+        this.props.history.push('/todos');
+
+    }
+
+
     render() {
         return (
             <div>
-                LOGIN PAGE!
+                <form onSubmit={this.handleSubmitLogin}>
+                Login to do Chores!!!
+                    <label>
+                        Email:
+                        <input
+                        onChange={(e) => this.setState({ email: e.target.value })}
+                        value={this.state.email} />
+                    </label>
+                    <label>
+                        Password:
+                        <input
+                        onChange={(e) => this.setState({ password: e.target.value })}
+                        value={this.state.password} type='password'/>
+                    </label>
+                    {
+                        this.state.loading
+                        ? '~ ~ ~ ~ ~ Loading ~ ~ ~ ~ ~'
+                        : <button>
+                            Log In!
+                        </button>
+                    }
+                </form>
             </div>
         )
     }
